4b96a4b6695bde73bc1b211d20e5816c2f0f8ccd
00ZRHun <00ZRHun@gmail.com>
2020-06-26 17:31:28 +0800

    adjust a bit

--- autoSender.js
+++ autoSender.js
@@ -28,27 +28,9 @@ windows.open("https://api.whatsapp.com/send?phone=601110604061&text=Hello%20Worl
 // openInChrome("https://google.com");
 // chrome.browser.openTab("https://abc.com/");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // var phoneList = ["601110604061", "601110851018", "60187843196"];
 var phoneList = [
-      /* "01115049981",
+  /* "01115049981",
       "0187856548",
       "0126063213",
       "0164225133",
@@ -79,7 +61,7 @@ var phoneList = [
       "0127017718",
       "0138169026",
       "65812288", */
-     /*  "0167646260",
+  /*  "0167646260",
       "0122691835",
       "0182484528",
       "0143132245",
@@ -91,87 +73,20 @@ var phoneList = [
       "01111409123",
       "0167991776" */
 
-
-
-
-
-
-
-      "0167646260",
-      "0122691835",
-      "0182484528",
-      "0143132245",
-      "0167771006",
-      "0182291885",
-      "0177081858",
-      "0127088879",
-      "0167055823",
-      "01111409123",
-      "0167991776",
-      "01111225039",
-      "0175127245",
-      "587524290"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  "0167646260",
+  "0122691835",
+  "0182484528",
+  "0143132245",
+  "0167771006",
+  "0182291885",
+  "0177081858",
+  "0127088879",
+  "0167055823",
+  "01111409123",
+  "0167991776",
+  "01111225039",
+  "0175127245",
+  "587524290",
 ];
 /* var phoneList = [
     "0123304159",
@@ -207,15 +122,15 @@ var phoneList = [
     "0196887808",
     "0177096683"
 ]; */
-var text = "%E6%82%A8%E5%A5%BD%F0%9F%91%8B%F0%9F%8F%BB%0A%E6%88%91%E6%98%AF%E8%8C%83%E7%A5%96%E8%8D%A3%0A%E8%A1%8C%E5%8A%A8%E7%AE%A1%E5%88%B6%E4%BB%A4%E5%B7%B2%E7%BB%8F%E5%BB%B6%E8%BF%9F%E5%88%B06%E6%9C%889%E6%97%A5%0A%E5%B8%8C%E6%9C%9B%E5%A4%A7%E5%AE%B6%E5%9C%A8%E8%BF%99%E4%B8%AA%E6%97%B6%E5%80%99%E8%83%BD%E5%A4%9F%E4%BF%9D%E6%8C%81%E5%81%A5%E5%BA%B7%0A%0A%E7%94%B1%E4%BA%8E%E6%88%91%E4%BB%AC%E6%94%B6%E5%88%B0%E8%9B%AE%E5%A4%9A%E4%BA%BA%E7%9A%84%E8%AF%A2%E9%97%AERequest%0A%E9%9C%80%E8%A6%81%E9%87%8D%E6%B8%A9%E6%9C%89%E5%85%B3UTMSpace%E5%85%B6%E4%B8%AD%E8%80%81%E5%B8%88%E7%9A%84Digital%20Marketing%E6%95%99%E5%AD%A6%0A%E6%89%80%E4%BB%A5%E6%88%91%E4%BB%AC%E4%BA%BA%E5%91%98%E4%BC%9A%E5%8F%91%0A%E6%9C%89%E5%85%B3%E5%85%8D%E8%B4%B990%E5%88%86%E9%92%9FVideo%E8%AF%BE%E7%A8%8B%E7%9A%84%E9%93%BE%E6%8E%A5%0A%E5%B8%8C%E6%9C%9B%E6%82%A8%E5%8F%AF%E4%BB%A5%E9%87%8D%E6%B8%A9%E6%9C%89%E5%85%B3%E7%9A%84Online%20Digital%20Marketing%E7%9F%A5%E8%AF%86%0A%E5%A6%82%E6%9E%9C%E9%9C%80%E8%A6%81%E5%85%B6%E4%BB%96%E5%85%8D%E8%B4%B9%E6%88%96%E4%BC%98%E6%83%A0%E8%AE%A4%E8%AF%81%E8%AF%BE%E7%A8%8B%E8%AF%B4%E6%98%8E%0A%E9%83%BD%E5%8F%AF%E4%BB%A5%E9%9A%8F%E6%97%B6%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC%F0%9F%99%82%0A%E8%B0%A2%E8%B0%A2%F0%9F%99%8F%0A%0A90%E5%88%86%E9%92%9F%E5%AE%9E%E6%88%98OnlineMarketing%E6%96%87%E6%A1%88%E6%95%99%E5%AD%A6%E5%88%86%E4%BA%AB%0Ahttps%3A%2F%2Fyoutu.be%2FSaoHCrjFlo0";
+var text =
+  "%E6%82%A8%E5%A5%BD%F0%9F%91%8B%F0%9F%8F%BB%0A%E6%88%91%E6%98%AF%E8%8C%83%E7%A5%96%E8%8D%A3%0A%E8%A1%8C%E5%8A%A8%E7%AE%A1%E5%88%B6%E4%BB%A4%E5%B7%B2%E7%BB%8F%E5%BB%B6%E8%BF%9F%E5%88%B06%E6%9C%889%E6%97%A5%0A%E5%B8%8C%E6%9C%9B%E5%A4%A7%E5%AE%B6%E5%9C%A8%E8%BF%99%E4%B8%AA%E6%97%B6%E5%80%99%E8%83%BD%E5%A4%9F%E4%BF%9D%E6%8C%81%E5%81%A5%E5%BA%B7%0A%0A%E7%94%B1%E4%BA%8E%E6%88%91%E4%BB%AC%E6%94%B6%E5%88%B0%E8%9B%AE%E5%A4%9A%E4%BA%BA%E7%9A%84%E8%AF%A2%E9%97%AERequest%0A%E9%9C%80%E8%A6%81%E9%87%8D%E6%B8%A9%E6%9C%89%E5%85%B3UTMSpace%E5%85%B6%E4%B8%AD%E8%80%81%E5%B8%88%E7%9A%84Digital%20Marketing%E6%95%99%E5%AD%A6%0A%E6%89%80%E4%BB%A5%E6%88%91%E4%BB%AC%E4%BA%BA%E5%91%98%E4%BC%9A%E5%8F%91%0A%E6%9C%89%E5%85%B3%E5%85%8D%E8%B4%B990%E5%88%86%E9%92%9FVideo%E8%AF%BE%E7%A8%8B%E7%9A%84%E9%93%BE%E6%8E%A5%0A%E5%B8%8C%E6%9C%9B%E6%82%A8%E5%8F%AF%E4%BB%A5%E9%87%8D%E6%B8%A9%E6%9C%89%E5%85%B3%E7%9A%84Online%20Digital%20Marketing%E7%9F%A5%E8%AF%86%0A%E5%A6%82%E6%9E%9C%E9%9C%80%E8%A6%81%E5%85%B6%E4%BB%96%E5%85%8D%E8%B4%B9%E6%88%96%E4%BC%98%E6%83%A0%E8%AE%A4%E8%AF%81%E8%AF%BE%E7%A8%8B%E8%AF%B4%E6%98%8E%0A%E9%83%BD%E5%8F%AF%E4%BB%A5%E9%9A%8F%E6%97%B6%E8%81%94%E7%B3%BB%E6%88%91%E4%BB%AC%F0%9F%99%82%0A%E8%B0%A2%E8%B0%A2%F0%9F%99%8F%0A%0A90%E5%88%86%E9%92%9F%E5%AE%9E%E6%88%98OnlineMarketing%E6%96%87%E6%A1%88%E6%95%99%E5%AD%A6%E5%88%86%E4%BA%AB%0Ahttps%3A%2F%2Fyoutu.be%2FSaoHCrjFlo0";
 // var phoneList = ["601110604061", "601110851018"];
 phoneList.forEach(myFunction);
-
 
 function myFunction(item, index) {
   // document.getElementById("someText").innerHTML +=  "https://api.whatsapp.com/send?phone=" + item + "&text=Hello%20World!%0AAnother%20World" + "</br>";
   // document.getElementById("someText").innerHTML +=  "<a id='anchor" +index+ "' target='_blank' href='" +　"https://api.whatsapp.com/send?phone=" + item + "&text=" +text+ "'>" + index + "</a>" + "</br>";
-  document.getElementById("someText").innerHTML +=  
+  document.getElementById("someText").innerHTML +=
     /* "</br>" + 
     "https://api.whatsapp.com/send?phone=6" + item + 
     "<a id='anchor" +index+ "' target='_blank' href='" +　"https://api.whatsapp.com/send?phone=" + item + "</a>" + 
@@ -224,15 +139,17 @@ function myFunction(item, index) {
     "https://api.whatsapp.com/send?phone=60" +item+  "&text=" +text+ 
     "<a id='anchor" +index+ "' target='_blank' href='" +　"https://api.whatsapp.com/send?phone=60" +item+  "&text=" +text+ "</a>" + 
     "</br>"; */
-    (index+1)+ "</br>" +
-    "https://api.whatsapp.com/send?phone=6" +item+ 
-    // "<a id='anchor" +index+ "' target='_blank' href='" +　"https://api.whatsapp.com/send?phone=6" +item+ "</a>" + 
+    index +
+    1 +
+    "</br>" +
+    "https://api.whatsapp.com/send?phone=6" +
+    item +
+    // "<a id='anchor" +index+ "' target='_blank' href='" +　"https://api.whatsapp.com/send?phone=6" +item+ "</a>" +
     "</br>";
   // location.href =  "https://api.whatsapp.com/send?phone=" + item + "&text=Hello%20World!%0AAnother%20World";
-//   document.getElementById("someText").innerHTML +=  "<a href=\"" + "https://api.whatsap.com/send?phone=" + item + "&text=Hello%20World!%0AAnother%20World" + "\">" + index + "</a></br>";
+  //   document.getElementById("someText").innerHTML +=  "<a href=\"" + "https://api.whatsap.com/send?phone=" + item + "&text=Hello%20World!%0AAnother%20World" + "\">" + index + "</a></br>";
   // document.getElementById("anchor" +index+ "").click();
 }
-
 
 // console.log()
 
